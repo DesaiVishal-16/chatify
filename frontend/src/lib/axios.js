91add91aefa2fd0db5afe243b706dff1158e7a1c
VishalDesai <desaidesai62230@gmail.com>
2025-08-11 16:20:32 +0530

    added backend url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5000/api"
-      : "/api",
+      : `${VITE_API_URL}/api`,
   withCredentials: true,
 });
